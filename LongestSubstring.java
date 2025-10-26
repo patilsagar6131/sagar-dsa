6572a573ec748e2644b1084731a90f414e7f71c9
Sagar Patil <sagarpatilte@gmail.com>
2025-10-26 09:47:14 +0530

    most optimized solution for two sum problem and length of biggest substring without repeating any char unoptimized code

---
+++ LongestSubstring.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Stack;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        var longestsubstring = new LongestSubstring();
+        System.out.println(longestsubstring.lengthOfLongestSubstring("pwwkew"));
+    }
+
+    // unoptimized code - accepeted.
+    // in this approach
+    // first an ans array is created to track max length of substring that can be
+    // achived from that position
+    // without repeating any character
+    // outer for loop travels 1 by 1 each characters of the string
+    // inner for loop adds the char to hashset and increase the length in ans array
+    // till it does not see a repeting char
+    // character and also increases ans by 1
+    // when inner loop sees that a character repeats it breaks
+    // outer loop goes to next character
+
+    public int lengthOfLongestSubstring(String s) {
+        int[] ans = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            var hs = new HashSet<Character>();
+
+            for (int j = i; j < s.length(); j++) {
+                if (!hs.contains(s.charAt(j))) {
+                    hs.add(s.charAt(j));
+                    ans[i] = ans[i] + 1;
+                } else {
+                    break;
+                }
+            }
+
+        }
+        System.out.println(Arrays.toString(ans));
+        int max = 0;
+        for (int k : ans) {
+            max = Math.max(k, max);
+        }
+        return max;
+    }
+
+}
