17178dd2c6751e48c5e2efe251f5d404cfb5813c
Sagar Patil <sagarpatilte@gmail.com>
2025-10-31 18:30:55 +0530

    "Optimized solution for longest panlindrome question"

--- LongestPalindromicString.java
+++ LongestPalindromicString.java
@@ -4,8 +4,54 @@ public class LongestPalindromicString {
     public static void main(String[] args) {
 
         var lognest = new LongestPalindromicString();
-        String s = "babadmadam";
-        System.out.println(lognest.longestPalindrome(s));
+        String s = "abccbc";
+        // System.out.println(lognest.longestPalindrome(s));
+        System.out.println(lognest.longestPalindrome1(s));
+    }
+
+    public String longestPalindrome1(String s) {
+        String res = "";
+        // orbit and axis method
+        // odd length palindromes
+        for (int axis = 0; axis < s.length(); axis++) {
+            // odd length palindromes - in odd cases starting orbit and length is always 1.
+            int orbit = 1;
+            int length = 1;
+            while ((axis - orbit) >= 0 && (axis + orbit) < s.length()) {
+                if (s.charAt(axis - orbit) == s.charAt(axis + orbit)) {
+                    orbit++;
+                    length = length + 2;
+
+                } else {
+                    break;
+                }
+            }
+
+            if (length > res.length()) {
+                int si = axis - length / 2;
+                res = s.substring(si, si + length);
+            }
+
+        }
+        // even number substring
+        for (int ax = 0; ax < s.length() - 1; ax++) {
+            int orbit = 1; // incase of even length length at starting is 0 and we compare with ax-orbit+1
+                           // chracter with ax+orbit
+            int length = 0;
+            while ((ax - orbit + 1) >= 0 && (ax + orbit) < s.length()) {
+                if (s.charAt(ax - orbit + 1) == s.charAt(ax + orbit)) {
+                    orbit++;
+                    length = length + 2;
+                } else {
+                    break;
+                }
+            }
+            if (length > res.length()) {
+                int si = ax - length / 2 + 1; // notice that length is also ax-length/2 +1 ;
+                res = s.substring(si, si + length);
+            }
+        }
+        return res;
     }
 
     // in this solution we have two for loops . Outer one trvaels full string once
