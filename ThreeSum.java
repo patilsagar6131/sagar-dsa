8aeb62c803b0752ed484d7df6f6db05dcaaa2209
Sagar Patil <sagarpatilte@gmail.com>
2025-11-13 17:57:48 +0530

    "3 sum"

--- ThreeSum.java
+++ ThreeSum.java
@@ -1,36 +1,52 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.IntStream;
 
 public class ThreeSum {
     public static void main(String[] args) {
+
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
         var ts = new ThreeSum();
-        int[] arr = { -1, 0, 1, 2, -1, -4 };
-        System.out.println(ts.threeSum1(arr));
+        System.out.println(ts.threeSum1(nums));
+
     }
 
-    public List<List<Integer>> threeSum1(int[] arr) {
-        Arrays.sort(arr);
-        Set<List<Integer>> uniquetriplets = new HashSet<>();
-        for (int i = 0; i < arr.length; i++) {
-            int j = i + 1;
-            for (int k = i + 2; k < arr.length; k++) {
-                if (arr[i] + arr[j] + arr[k] == 0) {
-                    var triplets = Arrays.asList(arr[i], arr[j], arr[k]);
-                    Collections.sort(triplets);
-                    uniquetriplets.add(triplets);
+    // optimized code
+    // in this approach we first sort the array and then select 1 num from nums
+    // array
+    // then set the pointer at left most and right most part
+    // calucate the new sum by substracting current outer loop num from target
+    // then check the new sum of li and ri digits if it is more we bring ri closer
+    // to li
+    // if it is less we bring li close to ri
+    // if sum is new sum then we add the 3 nums to list.
+    public List<List<Integer>> threeSum1(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> uniquetrLists = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int newTarget = 0 - nums[i];
+            int li = i + 1;
+            int ri = nums.length - 1;
+            while (li < ri) {
+                if (nums[li] + nums[ri] > newTarget) {
+                    ri--;
+                } else if (nums[li] + nums[ri] < newTarget) {
+                    li++;
+                } else {
+                    uniquetrLists.add((List.of(nums[i], nums[li], nums[ri])));
+                    li++;
+                    ri--;
                 }
             }
-
         }
-        List<List<Integer>> ans = new ArrayList<>(uniquetriplets);
-        return ans;
+        List<List<Integer>> list = new ArrayList<>(uniquetrLists);
 
+        System.out.println(list);
+        return list;
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
